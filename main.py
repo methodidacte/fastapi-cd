f93d780382da21699d6fa74ea12202c0ca0752c9
Paul PETON <paul.peton@live.fr>
2022-12-21 14:27:23 +0100

    without slot

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": "Application ran successfully - FastAPI with GitHub Actions"}
+    return {"data": "Application ran successfully - FastAPI without slot"}
   
   
 if __name__ == "__main__":
